53b581359416f0cc64e75007adc299eaee81e9a7
Adam Hałdaś <adam.haldas@gmail.com>
2022-08-18 11:26:05 +0200

    add react router and build nav

--- src/views/Root/App.js
+++ src/views/Root/App.js
@@ -1,17 +1,23 @@
-import './App.css';
-import {  Route, Routes,useRoutes} from 'react-router-dom';
+import './App.css'; 
+import {  Route, Routes} from 'react-router-dom';
 import HomeView from '../HomeView/HomeView';
 import ChatView from '../ChatView/ChatView';
 import ContactView from '../ContactView/ContactView';
+import Button from '../../components/Button/Button';
+import Navigation from '../../components/Navigation/Navigation';
 
 
 const  App =() =>  {
   return (
-    <Routes> 
-      <Route path='/'element={<HomeView/>}/>
-      <Route path='/chat' element={<ChatView/>}/>
-      <Route path='/contact' element={<ContactView/>}/>
-    </Routes>
+    <>
+    <Navigation/>
+    <Button>siema projekt</Button>
+  <Routes>
+    <Route path="/" element={<HomeView />}/>
+    <Route path="/chat" element={<ChatView />}/>
+    <Route path="/contact" element={<ContactView />}/>
+  </Routes>
+    </>
   );
 }
 
