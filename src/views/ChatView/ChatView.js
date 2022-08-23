690c1e33f25b1fbb7c83e142a68a391117eb3cd7
kolanowskim <kolanowskim99@gmail.com>
2022-08-24 00:17:36 +0200

    Added firestore and note in ChatView

--- src/views/ChatView/ChatView.js
+++ src/views/ChatView/ChatView.js
@@ -1,7 +1,10 @@
-import React from 'react'
-
+import React from "react";
 
 const HomeView = () => (
-    <h1>this is Chat view</h1>
-)
+  <h1>this is Chat view</h1>
+
+  //button create test group
+  //clickable list of groups redirecting to group id URL
+  //after redirect console log url
+);
 export default HomeView;
