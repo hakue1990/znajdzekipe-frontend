3fccfdcc027630763e436772666b5d4dfd456d2f
kolanowskim <kolanowskim99@gmail.com>
2022-08-27 14:33:01 +0200

    added .env file

--- firebase.js
+++ firebase.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAxEn9rW5oigdcWtQBDo5LwyF4UHCEXYSY",
-  authDomain: "szukamekipy-d023f.firebaseapp.com",
-  projectId: "szukamekipy-d023f",
-  storageBucket: "szukamekipy-d023f.appspot.com",
-  messagingSenderId: "502906486950",
-  appId: "1:502906486950:web:6c33940abfcbab9867b467",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
