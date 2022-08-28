1c027d155a417cc1f02d364f7fac92d4c8311e0e
kolanowskim <kolanowskim99@gmail.com>
2022-08-28 21:32:35 +0200

    added firebase login, user verification on ChatView

--- src/views/Root/App.js
+++ src/views/Root/App.js
@@ -1,20 +1,20 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import HomeView from '../HomeView/HomeView';
-import ChatView from '../ChatView/ChatView';
-import ContactView from '../ContactView/ContactView';
-import Navigation from '../../components/Navigation/Navigation';
-import ProjectView from '../ProjectView/ProjectView';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import HomeView from "../HomeView/HomeView";
+import ChatView from "../ChatView/ChatView";
+import ContactView from "../ContactView/ContactView";
+import Navigation from "../../components/Navigation/Navigation";
+import ProjectView from "../ProjectView/ProjectView";
 
 const App = () => {
   return (
     <>
       <Navigation />
       <Routes>
-        <Route path='/' element={<HomeView />} />
-        <Route path='/chat' element={<ChatView />} />
-        <Route path='/project' element={<ProjectView />} />
-        <Route path='/contact' element={<ContactView />} />
+        <Route path="/" element={<HomeView />} />
+        <Route path="/chat" element={<ChatView />} />
+        <Route path="/project" element={<ProjectView />} />
+        <Route path="/contact" element={<ContactView />} />
       </Routes>
     </>
   );
