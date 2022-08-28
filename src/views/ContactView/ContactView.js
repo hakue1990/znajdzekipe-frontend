c0fe0fbb244faf9a3c844289116247361b3e1919
Adam Hałdaś <adam.haldas@gmail.com>
2022-08-28 15:06:30 +0200

    add form to contact section

--- src/views/ContactView/ContactView.js
+++ src/views/ContactView/ContactView.js
@@ -1,23 +1,48 @@
-import React from 'react'
+import React from "react";
 
-
-import styled, { css } from 'styled-components';
-import ContactImg from '../../assets/images/ContactImg.webp';
+import styled, { css } from "styled-components";
+import ContactImg from "../../assets/images/ContactImg.webp";
 
 const ContactSection = () => (
   <>
     <Container>
       <Row column>
         <h1>
-          Szukasz Ekipy ?
+          napisz do <span style={{ color: "#f6ae2d" }}>nas</span>!
         </h1>
-        <h1 size='40px' margin='0 120px'>
-          Innowacyjna platforma, która pozwoli ci na robienie rzeczy{' '}
-          <span style={{ color: '#F6AE2D' }}>wspólnie</span>!
-        </h1>
+        <Form>
+          <InputContainer>
+            <Input type="text" id="dupa" required autoComplete="off" />
+            <Label htmlFor="dupa">
+              <Content>imie:</Content>
+            </Label>
+            <Line />
+          </InputContainer>
+          <InputContainer>
+            <Input type="text" id="dupa" required autoComplete="off" />
+            <Label htmlFor="dupa">
+              <Content>email:</Content>
+            </Label>
+            <Line />
+          </InputContainer>
+          <InputContainer>
+            <Input type="text" id="dupa" required autoComplete="off" />
+            <Label htmlFor="dupa">
+              <Content>temat:</Content>
+            </Label>
+            <Line />
+          </InputContainer>
+          <TextWrapper>
+            <Input type="text" id="dupa" required autoComplete="off" />
+            <Label htmlFor="dupa">
+              <TextAreaContent>wiadomość:</TextAreaContent>
+            </Label>
+            <Line />
+          </TextWrapper>
+        </Form>
       </Row>
       <Row>
-        <img src={ContactImg} alt='Contact' />
+        <img src={ContactImg} alt="Contact" />
       </Row>
     </Container>
   </>
@@ -62,6 +87,94 @@ const Row = styled.div`
     props.column &&
     css`
       flex-direction: column;
-      align-items: flex-start;
+      align-items: center;
     `}
+`;
+
+const Form = styled.form`
+  width: 500px;
+  height: 500px;
+  background-color: #fff;
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+  padding-top: 50px;
+  position: relative;
+  align-items: center;
+`;
+
+const InputContainer = styled.div`
+  width: 60%;
+  height: 50px;
+  background-color: blue;
+  position: relative;
+  overflow: hidden;
+  margin: 7px;
+`;
+
+const Content = styled.span`
+  position: absolute;
+  bottom: 5px;
+  left: 0;
+  transition: all 0.3s ease;
+`;
+
+const Label = styled.label`
+  position: absolute;
+  width: 300px;
+
+  max-width: 300px;
+  height: 100%;
+  pointer-events: none;
+  border-bottom: 3px solid black;
+  color: black;
+  font-size: 20px;
+`;
+
+const Line = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  transform: translateX(-100%);
+  border-bottom: 3px solid #5603ad;
+  transition: all 0.5s ease;
+`;
+const TextAreaContent = styled(Content)`
+`
+const Input = styled.input`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  outline: none;
+  border: none;
+  font-size: 20px;
+  overflow: hidden;
+  &:focus + ${Label} ${Content} {
+    transform: translateY(-20px);
+    color: #5603ad;
+    font-size: 13px;
+  }
+  &:valid + ${Label} ${Content} {
+    transform: translateY(-20px);
+    font-size: 13px;
+    color: #5603ad;
+  }
+  &:focus + ${Label} ${TextAreaContent} {
+    transform: translateY(-100px);
+    color: #5603ad;
+    font-size: 13px;
+  }
+  &:valid + ${Label} ${TextAreaContent} {
+    transform: translateY(-100px);
+    font-size: 13px;
+    color: #5603ad;
+  }
+  &:focus ~ ${Line} {
+    transform: translateX(0);
+  }
+`;
+
+const TextWrapper = styled(InputContainer)`
+height: 170px;
 `;
