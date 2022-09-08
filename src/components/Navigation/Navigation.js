cc535f9a4b41f582f560b09c1c018d090db51eab
Adam <adam.haldas@gmail.com>
2022-09-08 20:09:12 +0200

    add hamburger menu btn functional

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import LogoImg from "../../assets/images/logo.png";
-import { NavLink } from "react-router-dom";
-import Button from "../Button/Button";
-import { signInWithPopup, signOut } from "firebase/auth";
-import { auth, provider } from "../../firebase";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import LogoImg from '../../assets/images/logo.png';
+import { NavLink } from 'react-router-dom';
+import Button from '../Button/Button';
+import { signInWithPopup, signOut } from 'firebase/auth';
+import { auth, provider } from '../../firebase';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -29,23 +29,21 @@ const Navbar = () => {
 
   return (
     <Nav>
-      <MenuLink to="/">
-        <img src={LogoImg} alt="logo" />
+      <MenuLink to='/'>
+        <img src={LogoImg} alt='logo' />
       </MenuLink>
-      <Hamburger onClick={() => setIsOpen(!isOpen)}>
-        <span />
-        <span />
+      <Hamburger onClick={() => setIsOpen(!isOpen)} isOpen={isOpen}>
         <span />
       </Hamburger>
       <Menu isOpen={isOpen}>
-        <MenuLink to="/">Home</MenuLink>
-        <MenuLink to="/chat">Czat</MenuLink>
-        <MenuLink to="/search">Szukaj</MenuLink>
-        <MenuLink to="/contact">Kontakt</MenuLink>
-        <Button margin="20px 15px" onClick={signIn}>
+        <MenuLink to='/'>Home</MenuLink>
+        <MenuLink to='/chat'>Czat</MenuLink>
+        <MenuLink to='/search'>Szukaj</MenuLink>
+        <MenuLink to='/contact'>Kontakt</MenuLink>
+        <Button margin='20px 15px' onClick={signIn}>
           Zaloguj się
         </Button>
-        <Button margin="20px 15px" onClick={logOut}>
+        <Button margin='20px 15px' onClick={logOut}>
           Wyloguj się
         </Button>
       </Menu>
@@ -90,7 +88,7 @@ const Menu = styled.div`
   @media (max-width: 768px) {
     overflow: hidden;
     flex-direction: column;
-    max-height: ${({ isOpen }) => (isOpen ? "100vh" : "0")};
+    max-height: ${({ isOpen }) => (isOpen ? '100vh' : '0')};
     transition: max-height 0.3s ease-in;
     width: 100%;
   }
@@ -100,14 +98,46 @@ const Hamburger = styled.div`
   display: none;
   flex-direction: column;
   cursor: pointer;
-  span {
-    height: 2px;
-    width: 25px;
-    background: #7b7fda;
-    margin-bottom: 4px;
-    border-radius: 5px;
-  }
+
   @media (max-width: 768px) {
     display: flex;
+    span {
+      background-color: black;
+      position: relative;
+      transition: all 0.3s ease;
+      width: 30px;
+      height: 3px;
+      opacity: ${({ isOpen }) => (isOpen ? '0' : '1')};
+    }
+    transform: ${({ isOpen }) => (isOpen ? 'rotate(0)' : 'rotate(0)')};
+  }
+
+  &::before {
+    content: '';
+    height: 3px;
+    width: 100%;
+    background-color: white;
+    position: absolute;
+    top: 0;
+    left: 0;
+    transition: all 0.3s ease;
+
+    transform: ${({ isOpen }) =>
+      isOpen ? 'translateY(8px) rotate(45deg)' : 'translateY(-10px) rotate(0)'};
+
+
+  }
+  &::after {
+    content: '';
+    height: 3px;
+    width: 100%;
+    background-color: white;
+    position: absolute;
+    top: 0;
+    left: 0;
+    transition: all 0.3s ease;
+    transform: ${({ isOpen }) =>
+      isOpen ? 'translateY(8px) rotate(-45deg)' : 'translateY(0) rotate(0)'};
+  }
   }
 `;
