788f218b4b8a08f4bbab22582326667950edfe9e
Adam Hałdaś <adam.haldas@gmail.com>
2022-10-09 19:45:46 +0200

    add email.js

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,12 +1,14 @@
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
 import styled from "styled-components";
+import Button from "../Button/Button";
 
 const ContactForm = () => {
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
+    e.target.reset();
 
     emailjs
       .sendForm(
@@ -27,78 +29,127 @@ const ContactForm = () => {
 
   return (
     <Form ref={form} onSubmit={sendEmail}>
-      <TextBox>
+           
+      <InputContainer>
         <Input type="text" name="user_name" autoComplete="off" required />
         <Label>
-          <ContentText>Name</ContentText>
+          <Content>imie:</Content>
         </Label>
-      </TextBox>
+        <Line />
+      </InputContainer>
+      <InputContainer>
+        <Input type="text" name="user_title" autoComplete="off" required />
+        <Label>
+          <Content>tytuł:</Content>
+        </Label>
+        <Line />
+      </InputContainer>
+      <InputContainer>
+        <Input type="email" name="user_email" autoComplete="on" required />
+        <Label>
+          <Content>email</Content>
+        </Label>
+        <Line />
+      </InputContainer>
+      <TextWrapper>
+            <Input type="text" id="message" required autoComplete="off" name="message" />
+            <Label htmlFor="message">
+              <TextAreaContent>wiadomość:</TextAreaContent>
+            </Label>
+            <Line />
+          </TextWrapper>
 
-      {/* <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" /> */}
-      <input type="submit" value="Send" />
+      <Button>Wyślij </Button>
     </Form>
   );
 };
 export default ContactForm;
 
 const Form = styled.form`
-  width: 430px;
-  background-color: white;;
-  height: 530px;
+  width: 500px;
+  height: 540px;
+  background-color: #fff;
+  margin-top: 30px;
   display: flex;
   flex-direction: column;
-  padding: 20px;
-  align-items: center;
-  margin: 30px;
-`;
-
-const TextBox = styled.div`
-  width: 80%;
-  height: 30px;
-  background-color: #333;
+  padding-top: 50px;
   position: relative;
-  margin: 20px;
-  overflow-x: hidden;
+  align-items: center;
 `;
+ 
+const InputContainer = styled.div`
+  width: 60%;
+  height: 50px;
+  background-color: blue;
+  position: relative;
+  overflow: hidden;
+  margin: 7px;
+`;
+ 
+const Content = styled.span`
+  position: absolute;
+  bottom: 5px;
+  left: 0;
+  transition: all 0.3s ease;
+`;
+ 
+const Label = styled.label`
+  position: absolute;
+  width: 300px;
+ 
+  max-width: 300px;
+  height: 100%;
+  pointer-events: none;
+  border-bottom: 3px solid black;
+  color: black;
+  font-size: 20px;
+`;
+ 
+const Line = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  transform: translateX(-100%);
+  border-bottom: 3px solid #5603ad;
+  transition: all 0.5s ease;
+`;
+const TextAreaContent = styled(Content)`
+`
 const Input = styled.input`
   position: absolute;
-  left: 0;
-  top: 0;
   width: 100%;
   height: 100%;
   outline: none;
-  padding-top: 20px;
   border: none;
-`;
-
-const Label = styled.label`
-  position: absolute;
-  bottom: 0px;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  pointer-events: none;
-  border-bottom: 3px solid #FF7F11;
-  &::after {
-    content: "";
-    position: absolute;
-    left: 0;
-    bottom: -3px;
-    width: 100%;
-    height: 100%;
-    border-bottom: 3px solid #FF7F11;
-    transform: translate(-100%);
+  font-size: 20px;
+  overflow: hidden;
+  &:focus + ${Label} ${Content} {
+    transform: translateY(-20px);
+    color: #5603ad;
+    font-size: 13px;
+  }
+  &:valid + ${Label} ${Content} {
+    transform: translateY(-20px);
+    font-size: 13px;
+    color: #5603ad;
+  }
+  &:focus + ${Label} ${TextAreaContent} {
+    transform: translateY(-100px);
+    color: #5603ad;
+    font-size: 13px;
+  }
+  &:valid + ${Label} ${TextAreaContent} {
+    transform: translateY(-100px);
+    font-size: 13px;
+    color: #5603ad;
+  }
+  &:focus ~ ${Line} {
+    transform: translateX(0);
   }
 `;
-
-const ContentText = styled.span`
-    color: black;
-    position: absolute;
-    bottom: 5px;
-    left: 0px;
-    transition: all .3s ease;
-
-`
+ 
+const TextWrapper = styled(InputContainer)`
+height: 170px;
+margin-bottom: 30px;
+`;
