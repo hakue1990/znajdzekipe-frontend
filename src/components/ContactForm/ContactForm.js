1688fa18f2f8489b288115e62a265528bd3fe0ca
Adam Hałdaś <adam.haldas@gmail.com>
2022-10-09 18:58:00 +0200

    style contact form with styles components

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,30 +1,104 @@
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import styled from "styled-components";
 
- const ContactForm = () => {
+const ContactForm = () => {
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_38ye0dj', 'template_0qinh5x', form.current, 'JYFZJAGUVzxfHD-Im')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_38ye0dj",
+        "template_0qinh5x",
+        form.current,
+        "JYFZJAGUVzxfHD-Im"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
+    <Form ref={form} onSubmit={sendEmail}>
+      <TextBox>
+        <Input type="text" name="user_name" autoComplete="off" required />
+        <Label>
+          <ContentText>Name</ContentText>
+        </Label>
+      </TextBox>
+
+      {/* <label>Email</label>
       <input type="email" name="user_email" />
       <label>Message</label>
-      <textarea name="message" />
+      <textarea name="message" /> */}
       <input type="submit" value="Send" />
-    </form>
+    </Form>
   );
 };
 export default ContactForm;
+
+const Form = styled.form`
+  width: 430px;
+  background-color: white;;
+  height: 530px;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  align-items: center;
+  margin: 30px;
+`;
+
+const TextBox = styled.div`
+  width: 80%;
+  height: 30px;
+  background-color: #333;
+  position: relative;
+  margin: 20px;
+  overflow-x: hidden;
+`;
+const Input = styled.input`
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  outline: none;
+  padding-top: 20px;
+  border: none;
+`;
+
+const Label = styled.label`
+  position: absolute;
+  bottom: 0px;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  pointer-events: none;
+  border-bottom: 3px solid #FF7F11;
+  &::after {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: -3px;
+    width: 100%;
+    height: 100%;
+    border-bottom: 3px solid #FF7F11;
+    transform: translate(-100%);
+  }
+`;
+
+const ContentText = styled.span`
+    color: black;
+    position: absolute;
+    bottom: 5px;
+    left: 0px;
+    transition: all .3s ease;
+
+`
