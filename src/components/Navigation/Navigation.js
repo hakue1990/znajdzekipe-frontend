6fad070c7a9d134daf136d5bba4954e44181ead3
Adam <adam.haldas@gmail.com>
2022-10-12 18:20:32 +0200

    accepts master changes

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import LogoImg from '../../assets/images/logo.png';
@@ -6,25 +5,15 @@ import { NavLink } from 'react-router-dom';
 import Button from '../Button/Button';
 import { signInWithPopup, signOut } from 'firebase/auth';
 import { auth, provider } from '../../firebase';
-// import { useCookies } from 'react-cookie';
-=======
-import React, { useState } from "react";
-import styled from "styled-components";
-import LogoImg from "../../assets/images/logo.png";
-import { NavLink } from "react-router-dom";
-import Button from "../Button/Button";
-import { signInWithPopup, signOut } from "firebase/auth";
-import { auth, provider } from "../../firebase";
-import { getCookie } from "../../utils/getCookie";
-import { setCookie } from "../../utils/setCookie";
->>>>>>> michal
+import { getCookie } from '../../utils/getCookie';
+import { setCookie } from '../../utils/setCookie';
 
 const postData = (accessToken) => {
-  fetch("https://backend.szukamekipydo.pl/api/login", {
-    method: "POST",
-    mode: "cors",
+  fetch('https://backend.szukamekipydo.pl/api/login', {
+    method: 'POST',
+    mode: 'cors',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
       idToken: `${accessToken}`,
     },
   })
@@ -37,10 +26,6 @@ const postData = (accessToken) => {
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
-<<<<<<< HEAD
-  // const [cookies, setCookie] = useCookies(['cookie-name']);
-=======
->>>>>>> michal
 
   const signIn = () => {
     signInWithPopup(auth, provider)
@@ -65,36 +50,32 @@ const Navbar = () => {
 
   return (
     <Nav>
-      <MenuLink to="/">
-        <img src={LogoImg} alt="logo" />
+      <MenuLink to='/'>
+        <img src={LogoImg} alt='logo' />
       </MenuLink>
       <Hamburger onClick={() => setIsOpen(!isOpen)} isOpen={isOpen}>
         <span />
       </Hamburger>
       <Menu isOpen={isOpen}>
-        <MenuLink to="/" onClick={() => setIsOpen(false)}>
+        <MenuLink to='/' onClick={() => setIsOpen(false)}>
           Home
         </MenuLink>
-        <MenuLink to="/chat" onClick={() => setIsOpen(false)}>
+        <MenuLink to='/chat' onClick={() => setIsOpen(false)}>
           Czat
         </MenuLink>
-        <MenuLink to="/search" onClick={() => setIsOpen(false)}>
+        <MenuLink to='/search' onClick={() => setIsOpen(false)}>
           Szukaj
         </MenuLink>
-        <MenuLink to="/contact" onClick={() => setIsOpen(false)}>
+        <MenuLink to='/contact' onClick={() => setIsOpen(false)}>
           Kontakt
         </MenuLink>
-        <Button margin="20px 15px" onClick={signIn}>
+        <Button margin='20px 15px' onClick={signIn}>
           Zaloguj się
         </Button>
-        <Button margin="20px 15px" onClick={logOut}>
+        <Button margin='20px 15px' onClick={logOut}>
           Wyloguj się
         </Button>
-<<<<<<< HEAD
-        {/* {console.log(cookies)} */}
-=======
         {console.log(getCookie())}
->>>>>>> michal
       </Menu>
     </Nav>
   );
@@ -137,7 +118,7 @@ const Menu = styled.div`
   @media (max-width: 768px) {
     overflow: hidden;
     flex-direction: column;
-    max-height: ${({ isOpen }) => (isOpen ? "100vh" : "0")};
+    max-height: ${({ isOpen }) => (isOpen ? '100vh' : '0')};
     transition: max-height 0.3s ease-in;
     width: 100%;
   }
@@ -156,13 +137,13 @@ const Hamburger = styled.div`
       transition: all 0.3s ease;
       width: 30px;
       height: 3px;
-      opacity: ${({ isOpen }) => (isOpen ? "0" : "1")};
+      opacity: ${({ isOpen }) => (isOpen ? '0' : '1')};
     }
-    transform: ${({ isOpen }) => (isOpen ? "rotate(0)" : "rotate(0)")};
+    transform: ${({ isOpen }) => (isOpen ? 'rotate(0)' : 'rotate(0)')};
   }
 
   &::before {
-    content: "";
+    content: '';
     height: 3px;
     width: 100%;
     background-color: white;
@@ -172,10 +153,10 @@ const Hamburger = styled.div`
     transition: all 0.3s ease;
 
     transform: ${({ isOpen }) =>
-      isOpen ? "translateY(8px) rotate(45deg)" : "translateY(-10px) rotate(0)"};
+      isOpen ? 'translateY(8px) rotate(45deg)' : 'translateY(-10px) rotate(0)'};
   }
   &::after {
-    content: "";
+    content: '';
     height: 3px;
     width: 100%;
     background-color: white;
@@ -184,6 +165,6 @@ const Hamburger = styled.div`
     left: 0;
     transition: all 0.3s ease;
     transform: ${({ isOpen }) =>
-      isOpen ? "translateY(8px) rotate(-45deg)" : "translateY(0) rotate(0)"};
+      isOpen ? 'translateY(8px) rotate(-45deg)' : 'translateY(0) rotate(0)'};
   }
 `;
